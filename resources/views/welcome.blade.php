beb9925f25900b31ef3b032beb66d8f561141ccd
nafisFuad (cLUSTered) <bsse0920@iit.du.ac.bd>
2025-05-10 12:59:33 +0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -406,7 +406,7 @@
                 <div class="col-md-12">
                     <div class="mapouter">
                         <div class="gmap_canvas">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.0008612121765!2d90.39419149678953!3d23.747348700000018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b9fae2df6aa7%3A0x74b739a6f21cff5f!2sTeamcot%20Bangladesh%20Ltd!5e0!3m2!1sen!2sbd!4v1746855344611!5m2!1sen!2sbd" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.0008612121765!2d90.39419149678953!3d23.747348700000018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b9fae2df6aa7%3A0x74b739a6f21cff5f!2sTeamcot%20Bangladesh%20Ltd!5e0!3m2!1sen!2sbd!4v1746855344611!5m2!1sen!2sbd" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div>
                     </div>
                 </div>
